660bf0cabc36ad3f3fdbe480acd5a0dd36e2cb8f
Alessandro Cresto Miseroglio <alex179ohm@gmail.com>
2019-09-13 18:52:03 +0200

    Fix export_language

--- DeepSpeech/train_it.sh
+++ DeepSpeech/train_it.sh
@@ -59,7 +59,7 @@ pushd $HOME/ds/
             --export_dir /mnt/models/ \
             --export_tflite \
             --nouse_seq_length \
-            --export_language "ita"
+            --export_language "it"
     fi;
 
     if [ ! -f "/mnt/models/output_graph.pbmm" ]; then
