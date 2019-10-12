8c795ec074db39bce0f3de176e13f52e39735edb
mone27 <massaro.simone.it@gmail.com>
2019-10-12 14:37:30 +0200

    added evaluation lm to follow commonvoice-fr repo fix #16 (#22)
    * added evaluation lm to follow commonvoice-fr repo.
    
    * Move to DeepSpeech 0.6.0a9 (following french repo)
    
    * updated DeepSpeech branch and removed workaround in import_m-ailabs.sh

--- DeepSpeech/import_m-ailabs.sh
+++ DeepSpeech/import_m-ailabs.sh
@@ -7,9 +7,7 @@ pushd $HOME/ds/
 	fi;
 
 	if [ ! -f "/mnt/extracted/data/M-AILABS/it_IT/it_IT_train.csv" ]; then
-		# added --skiplist nothing to avoid issues with the importer TODO: find a proper solution
 		python bin/import_m-ailabs.py ${IMPORT_AS_ENGLISH} \
-		    --skiplist nothing\
 			--language it_IT                                \
 			/mnt/extracted/data/M-AILABS/
 	fi;
