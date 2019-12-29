3250dfd669f77f179989367681857e09c0405196
eziolotta <ezio.dinisio@gmail.com>
2019-12-29 10:33:57 +0100

    translate comment, bug fixing, add new regexp case for normalization

--- MITADS/opensubtitles_exporter.py
+++ MITADS/opensubtitles_exporter.py
@@ -6,89 +6,40 @@ from pathlib import Path
 from unidecode import unidecode  
 import time
 
+global start_time_regexp
 
 mapping_normalization = [
 
-  ##L'Ordine delle entry (replace e sub con regex) è significativo!!!
-
-  ##se la frase contiene parole con underscore o  @, allora non fa parte di una conversazione
-  ##[ re.compile('(^|(.*\s))\w+(_|@|®|\+|\{|\})\w+((\s.*)|$)'), u'' ],
-
-  ## If the sentence start with a number, the number is removed
+  ## If the sentence start with a number, the sentence is removed
   [ re.compile('^\d+(.*)'), u'' ],  
 
   ## Remove the stuff inside ()[]{}
   [ re.compile('(\(|\[|{)[^(\)|\]|})]*(\)|\]|})'), u'' ],
-
-  ## potrebbero essere parentesi annidate(ci sono nel dataset), lo lancio due volte
+  ##must be twice time for nested parentheses
   [ re.compile('(\(|\[|{)[^(\)|\]|})]*(\)|\]|})'), u'' ],
-
-  #### testo compreso tra separatori 
-  #[ re.compile('(\-|=)[^(\-|=)]*(\-|=)'), u'' ],
+ 
+  ##remove uninteresting characters
+  [ re.compile('\-|=|_|–|\+|\(|\||—|\)|\[|\]|~|\*|/|"|¨|\^'), u' ' ],     
   
-  ##replace caratteri, sono presenti nelle frasi
-  [ u'-' , u' ' ],
-  [ u'=' , u' ' ],
-  [ u'_' , u' ' ],
-  [ u'–' , u' ' ],##trattino differente  
-  [ u'+' , u'' ],
-  [ u'(' , u'' ],
-  [ u'|' , u'' ],
-  [ u'—' , u' ' ],  
-  [ u')' , u'' ],
-  [ u'[' , u'' ],
-  [ u']' , u'' ],
-  [ u'~' , u'' ],
-  [ u'=' , u'' ],  
-  [ u'*' , u'' ],  
-  [ u'/' , u' ' ],  
-  [ u'¢' , u'c' ],    
-  ##apici doppi , rimuovo sempre    
-  [ u'"' , u'' ],
-  [ u'¨' , u'' ],
-  [ u'^' , u'' ],
+  [ u'¢' , u'c' ],  
   
-  
-  ## Sanitize ... to .
+  ## Sanitize ... to . 
+  # Attention: Ellipsis are part of grammar, they are punctuation, they should not be eliminated
   [ re.compile('\.+'), u'.' ],
-  ##sigle e testi con punti
-  [ re.compile('\n|\t|\r'), u' ' ],
-  [ re.compile('\s+'), u' ' ],
+  ##normalize spaces
+  [ re.compile('\n|\t|\r|\s+'), u' ' ],
 
   ##accentate maiuscole
-  [ u'È' , u'è' ],
-  [ u'E\'\s' , u'è' ],  
+  [ re.compile('È'), u'e\'' ],
 
-  ##apostrofi, sono presenti 
-  [ u'´' , u'\'' ],
-  [ u'`' , u'\'' ],
-  [ u'\'\'' , u'\'' ],  
+  ##Sanitize single apex
+  [ re.compile('´|`|\'\''), u'\'' ],
 
   ## To avoid conflicts with single ' and accented letter we removed them
   [ re.compile('(\s|^)(\')([^\']*)(\')(\s|$)'), r'\3' ],
 
-  ## punteggiatura non significativa alla fine del testo  
-  [ re.compile('(.*)(\.)$'), r'\2' ],
-
-  ## Convert old fashion accented letter to the real accented letter
-  [ re.compile('a\'(\s|$|,|\.|\?)'), r'à\1' ],
-  [ re.compile('e\'(\s|$|,|\.|\?)'), r'è\1' ],
-  [ re.compile('i\'(\s|$|,|\.|\?)'), r'ì\1' ],
-  [ re.compile('o\'(\s|$|,|\.|\?)'), r'ò\1' ],
-  [ re.compile('u\'(\s|$|,|\.|\?)'), r'ù\1' ],
-
-  ## cancelletto con numero , presente nei titoli di apertura   
-  [ re.compile('#\d+'), u'' ],
-  [ re.compile('#|\s°'), u'' ], 
-
-  ##alcune normalizzazioni con numeri. sono presenti, vedere se serve questo tipo di normalizzazione
-  ##ISSUE: potrebbero esserci problemi con maschile e femminile, vedi 6° strada ad esempio
-  [ u'1°' , u'primo' ],
-  [ u'2°' , u'secondo' ],
-  [ u'3°' , u'terzo' ],
-  [ u'4°' , u'quarto' ],
-  [ u'5°' , u'quinto' ],
-  [ u'49°' , u'quarantanovesimo' ],
+  ## remove char for those cases
+  [ re.compile('(#\d+)|#|(\s°)'), u'' ],
   
   ## Sanitization for those cases
   [ u'n°' , u'numero ' ],  
@@ -100,13 +51,30 @@ mapping_normalization = [
   [ re.compile('([0-9]+[.,]{0,1}[0-9]*)\s*₤'), r'\1 lire' ],
   [ re.compile('(€)\s*([0-9]+[.,]{0,1}[0-9]*)'), r'\2 euro' ], 
   [ re.compile('([0-9]+[.,]{0,1}[0-9]*)\s*€'), r'\1 euro' ],
-  ##trim spazi
+
+  ##space trim - it must be reapplied again
   [ re.compile('\s+'), u' ' ]
+]
+
+
+mapping_normalization_after_decode = [
+
+  ## Convert old fashion accented letter to the real accented letter
+  [ u'E\' ', u'è ' ],
+  [ re.compile('a\'(\s|$|,|\.|\?)'), r'à\1' ], 
+  [ re.compile('e\'(\s|$|,|\.|\?)'), r'è\1' ],
+  [ re.compile('i\'(\s|$|,|\.|\?)'), r'ì\1' ],
+  [ re.compile('o\'(\s|$|,|\.|\?)'), r'ò\1' ],
+  [ re.compile('u\'(\s|$|,|\.|\?)'), r'ù\1' ],     
+
 ]
 
 
 def maybe_normalize(value, mapping=mapping_normalization):
   for norm in mapping:
+    if(value==''):
+      break
+
     if type(norm[0]) == str:
       value = value.replace(norm[0], norm[1])
     elif isinstance(norm[0], Pattern):
@@ -117,34 +85,33 @@ def maybe_normalize(value, mapping=mapping_normalization):
     else:
       print('UNEXPECTED', type(norm[0]), norm[0])
 
-  ##remove markup tags...(non sono presenti nel dataset )
-  #value = loads(value)
-
   return value
 
 def line_not_relevant(text):
-  ##implementare logiche di esclusione righe
+  
   if(len(text)<2):
     return True
 
+  
+  ## If the sentence include those symbols we ignore it (is part of the opening subtitles)  
+  if('®' in text
+     or '©' in text
+     or '±' in text):
+    return True
+
+  ##sentence with one word that contain those symbols, we ignore it
+  if(not ' ' in text and 
+    (   '_' in text
+     or '@' in text
+     or '}' in text     
+     or '+' in text
+     or '{' in text)):
+     return True
+
+  ##add other cases
+
   return False
-
-
-def clear_text(text):
-  ##text = re.sub(r'[^\x00-\x7F]+',' ', text)  mi toglie anche le È
-
-  #PER I CARATTERI UNICODE ho usato libreria unidecode, questi non servono più
-  #text = text.replace('\u200b',' ')## ZERO WIDTH SPACE
-  #text = text.replace(u'ε','e') ##GREEK SMALL LETTER EPSILON
-  #text = text.replace(u'♪','') ## char ♪
-  #text = text.replace(u'♫','') ## char ♫
-  #text = text.replace(u'¶','') ## char ♫
-  
-  #text = text.sub(r'[^\x00-\x7F]+',' ', text_ret)
-  
-  text = text.strip()
-
-  return text
+ 
 
 def normalize_text(text):
 
@@ -152,29 +119,31 @@ def normalize_text(text):
   if(text.isupper()):
     text = text.lower()
 
-  ##se una sentence contiene una sola parola con caratteri speciali 
-  # ALLORA tutta la sentence viene esclusa (fa  parte dei titoli di testa)
-  
-  if(not ' ' in text and 
-    (   '_' in text
-     or '@' in text
-     or '}' in text     
-     or '+' in text
-     or '{' in text)):
-     return ''
-  ## If the sentence include those symbols we ignore it
-  if('®' in text
-     or '©' in text
-     or '±' in text):
-    return ''
-
-  #normalization (replace and sub regex)
+  #normalization first step (replace and sub regex)
   text = maybe_normalize(text,mapping_normalization)
 
-  ######da spostare sulla clear?? vedere se spostando la chiamata non abbiamo side effect 
-  # unidecode normalizza i caratteri speciali ascii (altrimenti andrebbe in errore la fase di scrittura su file con utf-8)
-  text = unidecode(text)
-  #################
+  ###### UNICODE/NO-ASCII CHAR
+  ##Opensubtiles Dataset contains no-ASCII char
+  ## instead of process single unicode case we use unidecode package to delegate all unicode char processing
+  # to keep all vowels properly accented, and at the same time eliminate the other unicode characters, you need to use a substitution with place holders
+  ##abstent unicode placeholder
+  text =  text.replace('à', '<PH_A>')
+  text =  text.replace('è', '<PH_E>')
+  text =  text.replace('ì', '<PH_I>')
+  text =  text.replace('ò', '<PH_O>')
+  text =  text.replace('ù', '<PH_U>')
+
+  text = unidecode(text)## # Transliterate an Unicode object into an ASCII string
+  ###
+  text =  text.replace('<PH_A>','à')
+  text =  text.replace('<PH_E>','è')
+  text =  text.replace('<PH_I>','ì')
+  text =  text.replace('<PH_O>','ò')
+  text =  text.replace('<PH_U>','ù')
+  #############################################################
+  
+  ##normalization second step:  that produces output with unicode characters
+  text = maybe_normalize(text,mapping_normalization_after_decode)
 
   text = text.strip()
 
@@ -183,14 +152,13 @@ def normalize_text(text):
   #################################
   ##other normalization step
 
-  ##rimuovo il punto a pice all'inizio della sentence
+  ##remove dot and apex only if at the beginning of sentence
   if(text[0]=='.' or text[0]=='\''): 
     text = text[1:len(text)]
 
   if(len(text)==0):
     return ''
-
-  ##rimuovo apice alla fine della sentence, apici singoli già trattati. Quelli interni alla sentence potrebbero essere significativi
+  ##remove apex only if at the end of sentence
   if(text[len(text)-1]=='\''): 
     text = text[0:len(text)-1]  
   
@@ -199,7 +167,7 @@ def normalize_text(text):
   return text
 
 
-def preprocess_and_extract(folder,start_year=1920,split_output_file_rows=None):  
+def preprocess_and_extract(folder,start_year=1920,split_output_file_rows=None,check_sentence_cleaning=False):  
   """ Excract text from opensubtitles dataset
       preprocessing text, lear and normalization
 
@@ -209,6 +177,8 @@ def preprocess_and_extract(folder,start_year=1920,split_output_file_rows=None):
           :type start_year: int
           :param split_output_file_rows: Se diverso da None verranno generati più file di output ognuno con 'split_output_file_rows' numero di righe
           :type split_output_file_rows: int            
+          :param check_sentence_cleaning: Debug flag, check the goodness of the regexp applied. After the cleaning and normalization step, check that the sentence contains only 'typical char' of a conversation.
+          :type check_sentence_cleaning bool        
           :return: Nothing
           """
 
@@ -235,8 +205,17 @@ def preprocess_and_extract(folder,start_year=1920,split_output_file_rows=None):
   count_file = 0
   count_sentence = 0
   count_split_file = 1
+  global start_time_regexp
+  start_time_regexp = time.time() 
+
   for xml_file_path in xml_file_list:
     try:
+
+      ##decomment to test some sentence
+      #if(count_sentence>=6000):
+      #  break
+      ####################
+
       fp = open(xml_file_path,encoding='utf-8')
       
       prev_time_line = None
@@ -244,13 +223,9 @@ def preprocess_and_extract(folder,start_year=1920,split_output_file_rows=None):
       for line in fp:
         _line= line
 
-        #if('$' in _line):
-        #  deb = 0
-
         line=line.strip()
-        if(line=='' or len(line)<2):
+        if(line==''):
           continue
-
         ## If start with this symbol we ignore the line
         if(line[0]!='<'):
           ##clear and normalize
@@ -258,10 +233,10 @@ def preprocess_and_extract(folder,start_year=1920,split_output_file_rows=None):
           ##############
           if(line=='' or len(line)<2):
             continue
-
-          ##debug check - abilita se vuoi avere un output con le segnalazioni delle righe che richiedono attenzione o ulteriore pulizia
-          if(False):
+          
+          if(check_sentence_cleaning):
             regexp_apex_ok = re.compile(r'\w\'\w')
+            number_ok = re.compile(r'\d+°') ##example 1°, 2°,6°, 49°
             line_temp = line.replace(" ", "")
             if(not line_temp.isalnum() 
                 and '!' not in line_temp 
@@ -271,34 +246,34 @@ def preprocess_and_extract(folder,start_year=1920,split_output_file_rows=None):
                 and ',' not in line_temp
                 and ';' not in line_temp
                 #and '"' not in line_temp 
-                and not regexp_apex_ok.search(line_temp) ):
+                and not regexp_apex_ok.search(line_temp)
+                and not number_ok.search(line_temp) ):
               print('IMPROVE TEXT CLEANING: ' + line + ' - source: '+_line) 
           #####################################################
 
           ## Generate multiple files if split_output_file_rows is enabled
           if split_output_file_rows!=None and not (count_sentence % split_output_file_rows):
-            count_sentence = 0
+            #count_sentence = 0
             file_output.close()
-            file_output=open("opensubtiles_extracted_{}.txt".format(str(count_split_file)),"w")
+            new_filename = "opensubtiles_extracted_{}.txt".format(str(count_split_file))
+            file_output=open(new_filename,"w")
+            print('NEW FILE OUTPUT: ' + new_filename)
             count_split_file +=1
           ####
           try:
-            file_output.write(line+ '\n')
+            ##decomment if you want to check input vs output
+            #file_output.write(unidecode(_line))         
+               
+            file_output.write(line)
+            file_output.write('\n')
             count_sentence +=1
           except Exception as e:
-            ###QUESTO CODICE SI PUO' ELIMINARE _ ADESSO NON VA PIU' IN ERRORE
-            
-            print('ERROR WRITE ROW - '+str(e) + ' - processed: ' +line + ' - source: '+_line)
-            
-            ##remove all non-ascii char
-            line = re.sub(r'[^\x00-\x7F]+','', line)
-            file_output.write(line+ '\n')
-            count_sentence +=1
-            #raise(e)
+            raise(e)
           
           exported_file = True
 
-        ##prev_line to check delay time, todo.. 
+        ##If it is important to separate sentences based on waiting breaks, 
+        # decomment this... line with time tag start with '<time'
         #if(line.startswith('<time')):          
         #  prev_line = line
       
@@ -313,15 +288,17 @@ def preprocess_and_extract(folder,start_year=1920,split_output_file_rows=None):
 
     ##progress print
     if not (count_file % 100):
-      print (count_file, '/', total_files, 'file preprocessed')
+      print (count_file, '/', total_files, 'file processed')
 
   if(file_output!=None):
       file_output.close()
 
-
+  print ('\n')
+  print (count_sentence, ' sentences extracted.' )
+  
 def process_text(text):
-  ##clear text 
-  text = clear_text(text)
+  ##clear and normalize text
+  text = text.strip()
 
   ##exclude line not relevant
   if(line_not_relevant(text)):
@@ -333,19 +310,58 @@ def process_text(text):
   return text
 
 def test_regexp():
-  ##£ 500 in premio per i ladri di cavalli
-  text = "{\pos(346372)}Agisco senza emozione"
-  text =  process_text(text)
+  
+  #test cases found in opensubtitles dataset.
 
-  iii=0
+  #this function could be used for a test case of text processing (clear and normalization),
+  #to be implemented better...
+
+  test_pair = []
+  test_pair.append(['Per cominciare... la civilta\' moderna e\' una truffa.','Per cominciare. la civiltà moderna è una truffa.'])
+  test_pair.append(['£ 500 in premio per i ladri di cavalli','500 lire in premio per i ladri di cavalli'])
+  test_pair.append(['{\pos(346372)}Agisco senza emozione','Agisco senza emozione'])
+  test_pair.append(["Si', e' cosi'.",'Sì, è così.'])
+  test_pair.append(["Si'... ",'Sì.'])
+  test_pair.append(["testo ddjd(( -- )) - [ ok... ",'testo ddjd ok.'])
+  test_pair.append(["Com'e?",'Com\'è?'])
+ 
+
+  succ = True
+  for pair in test_pair:
+    text = pair[0]
+    text_res =  process_text(text)
+    text_expected = pair[1]
+    if(text_res!=text_expected):
+      print('error on text: {}'.format(text))
+      print('output produced: {}'.format(text_res))
+      print('output expected: {}'.format(text_expected)) 
+      print('\n')
+      succ = False
+  
+  if(succ):
+    print('Processing successfully!!!!')
 
 if __name__ == '__main__':
 
+  ##decomment to run some test case on regexp expression
   #test_regexp()
- 
+  #############################
+
   folder_dataset = '../../../../../data_and_models/dataset/public_opensubtitle_sottotitoli_it_2018/it/OpenSubtitles/raw/it'
-  start_time = time.time()
-  
-  preprocess_and_extract(folder_dataset)
+  start_year=1920
+
+  ##the procedure produces a text file of several gigabytes.
+  # If you want to have files that can be easily explored with Notepad++/Sublime I recommend splitting the output files into several files of about 1G each (about 34Milion sentence by file)
+  split_output_file_rows = 34000000 ## 34 milion rows , 1G file txt
+
+  #########################################
+
+  start_time = time.time()  
+
+  preprocess_and_extract(folder_dataset,start_year=start_year,split_output_file_rows=split_output_file_rows)
 
   print("--- %s seconds duration ---" % (time.time() - start_time))
+
+  global start_time_regexp
+  print("--- %s seconds duration regexp parsing ---" % (time.time() - start_time_regexp))
+  
