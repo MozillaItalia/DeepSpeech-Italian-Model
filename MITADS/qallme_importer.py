a0680b6c5bbe6f574ecb78dc7f8b834c93eb19ea
Daniele Scasciafratte <mte90net@gmail.com>
2020-04-13 17:06:30 +0200

    missing shebang and code optimization

--- MITADS/qallme_importer.py
+++ MITADS/qallme_importer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import os
 import xml.etree.ElementTree as ET
 from utils import sanitize, line_rules, download
@@ -26,54 +27,28 @@ filename = "qallme.txt"
 
 output_file = open(outdir + filename, "w", encoding='utf-8')
 
-# print("\n")
 print("Qallme Importer")
-print("===============")
-# downloading resource
-download_link = 'http://qallme.fbk.eu/archive/QB_IT_V1.0_TranscriptionsReferences.zip'
 
+download_link = 'http://qallme.fbk.eu/archive/QB_IT_V1.0_TranscriptionsReferences.zip'
 downloader = download.Download()
 downloader = downloader.if_not_exist(download_link)
-
-# extracting files
 downloader.zip_decompress(parsedir)
 
-# going to the right directory
-os.chdir(parsedir + "QB_IT_V1.0_Translations")  # name of the folder inside
-                                                # the zip package
-
-
 ###  XML  ###
-qallmef = ET.parse("QallmebenchmarkIT_v1.0_final-translation.xml")
-
+qallmef = ET.parse(parsedir + "QB_IT_V1.0_Translations/QallmebenchmarkIT_v1.0_final-translation.xml")
 sentences = qallmef.findall("question/text")
-len_sentences = len(sentences)
-
-print("Now parsing " + str(len_sentences) + " sentences... ")
 
 # We are looking for sentences, not xml elements!
 # turning xml elements into real sentences
-i = 0
 for s in sentences:
-    sentences[i] = sentences[i].text
-    i += 1
-
-# sanitizing line by using libs
-for line in sentences:    
-    if line is not None:    # if we are not treating an empty line
-        line = sanitizer.maybe_normalize(line, mapping_normalization)
-
-# print("OK!")
-
-
-print("Now writing to " + outdir + filename + "... ")
-
-# writing to output file
-for line in sentences:
+    line = s.text
     if line is not None:
+        line = sanitizer.maybe_normalize(line, mapping_normalization)
         output_file.write(line)
         output_file.write("\n")
 
-#print("OK!\n")
-
+output_file.close()
 print("Import from QALLME completed!")
+result = open(outdir + filename, 'r')
+print(' Total lines: ' + str(len(result.read().splitlines())))
+result.close()
