0556a961069d0400b930b343c6bc0a7bf27ae309
Daniele Scasciafratte <mte90net@gmail.com>
2020-04-15 22:50:18 +0200

    fix download qallme

--- MITADS/qallme_importer.py
+++ MITADS/qallme_importer.py
@@ -19,20 +19,16 @@ mapping_normalization = [
 ]
 
 # managing parse directory name
-parsedir = "parsing" + os.path.sep
+parsedir = "parsing/qall"
 
 # managing output pathname + output filename
-outdir = "output" + os.path.sep
-filename = "qallme.txt"
+output = "output/qallme.txt"
 
-output_file = open(outdir + filename, "w", encoding='utf-8')
+output_file = open(output, "w", encoding='utf-8')
 
 print("Qallme Importer")
 
-download_link = 'http://qallme.fbk.eu/archive/QB_IT_V1.0_TranscriptionsReferences.zip'
-downloader = download.Download()
-downloader = downloader.if_not_exist(download_link)
-downloader.zip_decompress(parsedir)
+downloader = download.Download().if_not_exist('http://qallme.fbk.eu/archive/QB_IT_V1.0_TranscriptionsReferences.zip').zip_decompress(parsedir)
 
 ###  XML  ###
 qallmef = ET.parse(parsedir + "QB_IT_V1.0_Translations/QallmebenchmarkIT_v1.0_final-translation.xml")
@@ -49,6 +45,6 @@ for s in sentences:
 
 output_file.close()
 print("Import from QALLME completed!")
-result = open(outdir + filename, 'r')
+result = open(output, 'r')
 print(' Total lines: ' + str(len(result.read().splitlines())))
 result.close()
--- MITADS/utils/download.py
+++ MITADS/utils/download.py
@@ -60,7 +60,7 @@ class Download:
         if file == '':
             file = self.file
             
-        if os.path.isdir(extract_to):
+        if not os.path.isdir(extract_to):
             print('Decompressing to ' + extract_to)
             with zipfile.ZipFile(self.file, "r") as zip_ref:
                 zip_ref.extractall(extract_to)
