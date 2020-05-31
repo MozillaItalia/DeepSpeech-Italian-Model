b01cb79d88d662196924ef6d4f437ba8f6ee2a06
nefastosaturo <10575994+nefastosaturo@users.noreply.github.com>
2020-05-31 18:48:41 +0200

    clean ted and notebooks (#70)
    clean ted importer output (need a review)
    add colab notebooks for DS 0.7 training and language model

--- MITADS/utils/sanitize.py
+++ MITADS/utils/sanitize.py
@@ -1,46 +1,21 @@
 from typing.re import Pattern
 import roman
 import re
+import utils.blacklist as blacklist
 from bs4 import Tag, NavigableString
 
 class Sanitization:
     """Various methods to clean the text to be used with DeepSpeech"""
-    
-    def maybe_normalize(self, value, mapping='', roman_normalization=True):
-      default_mapping = [
-              [ u'«', u'' ],
-              [ u'»', u'' ],
-              [ u'×' , u'' ],
-              [ u'_' , u'' ],
-              [ u'-' , u'' ],
-              [ u'—' , u'' ],
-              [ u'* * * ' , u'' ],
-              [ u'↑' , u'' ],
-              [ u'( ' , u'' ],
-              [ u' , ' , u', ' ],
-              [ u' )' , u'' ],
-              [ u' .' , u'.' ],
-              [ u'\' ' , u'\'' ],
-              [ u' !' , u'!' ],
-              [ u' ?' , u'?' ],
-              [ u'\'\'' , u'' ],
-              [ u'“', u'"'],
-              [ u'”', u'"'],
-              [ u'’', u'\''],
-              [ u'" "', u'"'],              
-              [ r'^https?:\/\/.*[\r\n]*', '' ],
-              [ r'^http?:\/\/.*[\r\n]*', '' ],
-              [ r'(^[ \t]+|[ \t]+(?=:))', '' ],
-              [ u'Sig. ', u'Signor ' ],
-              [ u'Sr. ', u'Signor ' ],
-              [ u'Sra. ', u'Signora ' ],
-              [ re.compile('\[\d+\]'), u'' ],
-            ]
+    def __init__(self):
+        self.default_mapping = blacklist.unicode_symbols + blacklist.other
+
+    def maybe_normalize(self, value, mapping='', roman_normalization=True, mapping_append=True):
+
       if mapping == '':
-          mapping = default_mapping
+          mapping = self.default_mapping
       else:
-          mapping = mapping + default_mapping
-          
+          mapping = mapping + self.default_mapping if mapping_append else self.default_mapping+mapping
+
       for norm in mapping:
         if type(norm[0]) == str:
           value = value.replace(norm[0], norm[1])
@@ -48,7 +23,7 @@ class Sanitization:
           value = norm[0].sub(norm[1], value)
         else:
           print('UNEXPECTED', type(norm[0]), norm[0])
-    
+
       if roman_normalization:
           for ro_before, ro_after, ro in self.get_roman_numbers(value):
             try:
@@ -56,12 +31,12 @@ class Sanitization:
             except roman.InvalidRomanNumeralError as ex:
               print(ex)
               pass
-    
+
       value = self.clean_single_line(value)
-      
+
       return value.replace('  ', " ")
-    
-    
+
+
     def get_roman_numbers(self, ch):
       ROMAN_CHARS = "XVI"
       ro  = ''
@@ -81,25 +56,26 @@ class Sanitization:
               yield ch[ros-1], ch[i], ro
             ro  = ''
             ros = i
-    
+
       if len(ro) > 0:
         yield ch[ros-1], '', ro
-        
+
     def prepare_splitlines(self, text):
         text = text.replace('. ', ".\n")
         text = text.replace('... ', "\n")
         text = text.replace('? ', "\n")
         text = text.replace('! ', "\n")
         text = text.replace('– ', "\n")
+        text = text.replace('─ ', "\n")
         text = text.replace('""', '"' + "\n" + '"')
-        
+
         if isinstance(text, Tag):
             return text.prettify()
         elif isinstance(text, NavigableString):
             return text
         else:
             return text
-    
+
     def escapehtml(self, text, html_escape_table=''):
         if html_escape_table == '':
             html_escape_table = {
@@ -111,20 +87,20 @@ class Sanitization:
                  "&Egrave;": "È",
                  }
         return "".join(html_escape_table.get(c,c) for c in str(text))
-    
-    def clean_single_line(self, value): 
+
+    def clean_single_line(self, value):
       if value.startswith(';') or value.startswith('–') or value.startswith('.') or value.startswith(':') or value.startswith('\'') or value.startswith('*') or value.startswith(') ') or value.startswith('< ') or value.startswith(',') or value.startswith('-'):
           value = value[1:]
 
       if value.startswith('"') and value.endswith('"'):
           value = value.replace('"', '')
-          
+
       if value.endswith('–') or value.endswith('*'):
           value = value[:-1]
 
       if value.count('"') == 1:
           value = value.replace('"', "")
-          
+
       if(value.isupper()):
           value = value.lower()
 
