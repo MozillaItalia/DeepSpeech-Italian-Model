d57d4e558fc0f90f1f45f3cec9232e8bb50fadb1
Daniele Scasciafratte <mte90net@gmail.com>
2020-07-09 18:44:03 +0200

    fix bz2_decompress not extracting

--- MITADS/utils/download.py
+++ MITADS/utils/download.py
@@ -12,7 +12,7 @@ from bs4 import BeautifulSoup
 class Download:
     file = ''
     folder = './parsing/'
-    
+
     def download_status(self, count, block_size, total_size):
         # from https://blog.shichao.io/2012/10/04/progress_speed_indicator_for_urlretrieve_in_python.html
         global start_time
@@ -26,7 +26,7 @@ class Download:
         sys.stdout.write("\r...%d%%, %d MB, %d KB/s, %d seconds passed" %
                         (percent, progress_size / (1024 * 1024), speed, duration))
         sys.stdout.flush()
-    
+
     def if_not_exist(self, url):
         self.file = self.folder + url.rsplit('/', 1)[-1]
         if not os.path.isfile(self.file):
@@ -40,53 +40,52 @@ class Download:
                 print('  Encountered unknown error. Continuing.')
         else:
             print('File already downloaded ' + self.file)
-        
+
         return self
-    
+
     def tarbz2_decompress(self, extract_to="", file=''):
         if file == '':
             file = self.file
-            
+
         if os.path.isfile(extract_to):
             print('Decompressing to ' + extract_to)
-            tar = tarfile.open(file, "r:bz2")  
+            tar = tarfile.open(file, "r:bz2")
             tar.extractall(extract_to)
             tar.close()
-                
+
         return extract_to
-    
+
     def bz2_decompress(self, file=''):
         if file == '':
             file = self.file
-            
-        extract_to = self.file.replace('.bz2','')
-        if os.path.isfile(extract_to):
-            print('Decompressing to ' + extract_to)
-            with open(extract_to, 'wb') as new_file, bz2.BZ2File(self.file, 'rb') as file:
-                for data in iter(lambda : file.read(100 * 1024), b''):
-                    new_file.write(data)
-                
+
+        extract_to = self.file.replace('.bz2', '')
+        print('Decompressing to ' + extract_to)
+        with open(extract_to, 'wb') as new_file, bz2.BZ2File(self.file, 'rb') as file:
+            for data in iter(lambda: file.read(100 * 1024), b''):
+                new_file.write(data)
+
         return extract_to
-    
+
     def zip_decompress(self, extract_to="", file=""):
         if file == '':
             file = self.file
-            
+
         if not os.path.isdir(extract_to):
             print('Decompressing to ' + extract_to)
             with zipfile.ZipFile(self.file, "r") as zip_ref:
                 zip_ref.extractall(extract_to)
-                
+
         return extract_to
-    
+
     def download_page(self, link, decode='UTF-8'):
         ua = UserAgent()
-        
+
         response = Request(link, headers={'User-Agent': ua.random})
         response = urlopen(response).read()
         return response.decode(decode).strip()
-    
+
     def download_for_bp(self, link, decode='UTF-8'):
         soup = BeautifulSoup(self.download_page(link, decode), 'html.parser')
-        return soup    
-        
+        return soup
+
