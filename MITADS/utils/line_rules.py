abf40173cc2aee2062f134b0c0a27066b088c6fe
mone27 <massaro.simone.it@gmail.com>
2020-07-13 12:49:22 +0200

    Added ability to save discarded sentences, useful for debugging

--- MITADS/utils/line_rules.py
+++ MITADS/utils/line_rules.py
@@ -1,43 +1,68 @@
 import re
-valid_char_regex = re.compile(r"^\s'abcdefghijklmnopqrstuvwxyzàèéìíòóôùúABCDEFGHIJKLMNOPQRSTUVWXYZÀÈÉÌÍÒÓÔÙÚ,\.!?:;")
+import json
+valid_char_regex = re.compile(r"[^\s'abcdefghijklmnopqrstuvwxyzàèéìíòóôùúABCDEFGHIJKLMNOPQRSTUVWXYZÀÈÉÌÍÒÓÔÙÚ,.!?:;]")
+
 
 class LineRules:
     """Rules to identify a valid line"""
 
+    def __init__(self, discard_file=None):
+        """provide a file path to have a place where to save discarded sentences"""
+        self.discard_file = discard_file
+        if discard_file:
+            self.discarded = {}
+
+    def discard(self, rule, line):
+        if self.discard_file:
+            self.discarded.setdefault(rule, []).append(line)
+
+    def save(self):
+        with open(self.discard_file, 'w') as f:
+            json.dump(self.discarded, f)
+
     def is_not_valid(self, line):
-        return valid_char_regex.match(line)
+        not_valid = valid_char_regex.search(line)
+        if not_valid: self.discard("is_not_valid", line)
+        return not_valid
     
     def startswith(self, line, chars):
         for char in chars:
             if line.startswith(char):
+                self.discard("startswith_" + str(chars) , line)
                 return True
             
     def endswith(self, line, chars):
         for char in chars:
             if line.endswith(char):
+                self.discard("endswith_" + str(chars), line)
                 return True
     
     def contain(self, line, chars):
         for char in chars:
             if line.find(char) >= 0:
+                self.discard("contain" + str(chars), line)
                 return True
     
     def isdigit(self, searchme):
         for search in searchme:
             if search.isdigit():
+                self.discard("is_digit", search)
                 return True
             
     def isbookref(self, text):
         text = text.replace('.', '')
         if text.find(',') >= 2 and text[-2:].isdigit():
+            self.discard("isbookref", text)
             return True
         
     def isbrokensimplebracket(self, text):
         if text.find('(') >= 1 and text.find(')') == -1:
+            self.discard("isbrokensimplebracket", text)
             return True
         
     def isbrokenparenthesis(self, text):
         if text.find(')') >= 1 and text.find('(') == -1 or text.find('(') >= 1 and text.find(')') == -1:
+            self.discard("isbrokenparenthesis", text)
             return True
         
     def hasafinalrepeated(self, text):
@@ -45,6 +70,7 @@ class LineRules:
             if len(word) >= 2 and not word.strip().isnumeric():
                 end = word[-3:]
                 if len(end.replace(end[0], '')) == 0:
+                    self.discard("hasafinalrepeated", text)
                     return True
         return False
     
@@ -61,4 +87,6 @@ class LineRules:
                     last = char
                 elif last is None or last == ')':
                     return False
+
+        self.discard("parenthesismatch", text)
         return True
--- MITADS/wikiquote_exporter.py
+++ MITADS/wikiquote_exporter.py
@@ -9,9 +9,10 @@ from utils import sanitize, line_rules, download
 
 DOWNLOAD_PATH = 'https://dumps.wikimedia.org/itwikiquote/latest/itwikiquote-latest-pages-articles.xml.bz2'
 OUTFILE = "output/wikiquote.txt"
+DISCARD_FILE = 'output/discarded/wikiquote.json'
 
 download_me = download.Download()
-validate_line = line_rules.LineRules()
+validate_line = line_rules.LineRules(discard_file=DISCARD_FILE)
 clean_me = sanitize.Sanitization()
 
 sub_regex = re.compile(r"""\[\[(File|Category):[\s\S]+\]\]|
@@ -35,24 +36,26 @@ normalize_rules = [['*', u"\n"],
 
 def process_page(page, out_file):
     title = page.getElementsByTagName("title")[0].firstChild.data
-
+    tot_lines = 0
     if 'wiki' not in title and title != 'Pagina principale' and 'MediaWiki' not in title:
         textdom = page.getElementsByTagName("revision")[0].getElementsByTagName("text")[0]
         if textdom.firstChild is not None:
-            text = ''
             raw_text = unescape(textdom.firstChild.data)
             raw_text = sub_regex.sub("", raw_text)
             raw_text = clean_me.maybe_normalize(raw_text, normalize_rules, False)
             lines = clean_me.prepare_splitlines(raw_text).splitlines()
             for line in lines:
-                process_line(line, out_file)
-            return len(lines)
-    return 0  # the number of lines
+                if process_line(line, out_file):
+                    tot_lines += 1
+    return tot_lines
 
 
 def process_line(line, out_file):
     """if line is invalid returns early, if is correct writes the line to the file"""
-    line = clean_me.clean_single_line(line).strip()
+    # line = clean_me.clean_single_line(line).strip()
+    # # to debug strange sentences
+    # if "L'uomo o impazzisce o scrive" in line:
+    #     print(line)
     if (validate_line.is_not_valid(line) or
             len(line) <= 15 or
             validate_line.startswith(line, ['(', 'vivente)']) or
@@ -60,9 +63,10 @@ def process_line(line, out_file):
             validate_line.isdigit([line, line[1:], line[:1]]) or
             validate_line.isbookref(line) or
             validate_line.isbrokensimplebracket(line)):
-        return
+        return False
     else:
         out_file.write(line + "\n")
+        return True
 
 
 def main():
@@ -81,6 +85,7 @@ def main():
 
     print("Total number of line processed: ", tot_lines)
 
+    validate_line.save()
 
 if __name__ == "__main__":
     main()
