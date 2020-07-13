bc84b925466039d3eb0fad653a90272e136573f6
nefastosaturo <10575994+nefastosaturo@users.noreply.github.com>
2020-07-13 15:44:58 +0200

    add xml format check for text/x-wiki nodes to avoid plain/text like code snippets (need test)

--- MITADS/wikiquote_exporter.py
+++ MITADS/wikiquote_exporter.py
@@ -21,55 +21,57 @@ text = ''
 for elem in items:
     title = elem.getElementsByTagName("title")[0].firstChild.data
     if 'wiki' not in title and title != 'Pagina principale' and 'MediaWiki' not in title:
-        textdom = elem.getElementsByTagName("revision")[0].getElementsByTagName("text")[0]
-        if textdom.firstChild is not None:
-            text = ''
-            raw_text = unescape(textdom.firstChild.data)
-            raw_text = re.compile(r"""\[\[(File|Category):[\s\S]+\]\]|
-                        \[\[[^|^\]]+\||
-                        \[\[|\]\]|
-                        \'{2,5}|
-                        (<s>|<!--)[\s\S]+(</s>|-->)|
-                        (<s>|<!)[\s\S]+(</s>|>)|
-                        {{[\s\S\n]+?}}|
-                        <.*?>|
-                        ={1,6}""", re.VERBOSE).sub("", raw_text)
-            raw_text = clean_me.maybe_normalize(raw_text, [
-                      [ u'*' , u"\n" ],
-                      [ u'<br />' , u"\n" ],
-                      [ u'<br>' , u"\n" ],
-                      [ u"\(\d\d\d\d\)", ""],
-                      [ u"[\(\[].*?[\)\]]", ""],
-                      [ 'AvvertenzaContattiDonazioni', '']
-                    ], False)
-            raw_text = clean_me.prepare_splitlines(raw_text).splitlines()
+        format = elem.getElementsByTagName("revision")[0].getElementsByTagName("format")[0]
+        if format.firstChild.data == 'text/x-wiki':
+            textdom = elem.getElementsByTagName("revision")[0].getElementsByTagName("text")[0]
+            if textdom.firstChild is not None:
+                text = ''
+                raw_text = unescape(textdom.firstChild.data)
+                raw_text = re.compile(r"""\[\[(File|Category):[\s\S]+\]\]|
+                            \[\[[^|^\]]+\||
+                            \[\[|\]\]|
+                            \'{2,5}|
+                            (<s>|<!--)[\s\S]+(</s>|-->)|
+                            (<s>|<!)[\s\S]+(</s>|>)|
+                            {{[\s\S\n]+?}}|
+                            <.*?>|
+                            ={1,6}""", re.VERBOSE).sub("", raw_text)
+                raw_text = clean_me.maybe_normalize(raw_text, [
+                          [ u'*' , u"\n" ],
+                          [ u'<br />' , u"\n" ],
+                          [ u'<br>' , u"\n" ],
+                          [ u"\(\d\d\d\d\)", ""],
+                          [ u"[\(\[].*?[\)\]]", ""],
+                          [ 'AvvertenzaContattiDonazioni', '']
+                        ], False)
+                raw_text = clean_me.prepare_splitlines(raw_text).splitlines()
 
-            for line in raw_text:
-                line = clean_me.clean_single_line(line).strip()
+                for line in raw_text:
+                    line = clean_me.clean_single_line(line).strip()
 
-                if len(line) <= 15:
-                    continue
+                    if len(line) <= 15:
+                        continue
 
-                if validate_line.startswith(line, ['(', 'vivente)']):
-                    continue
+                    if validate_line.startswith(line, ['(', 'vivente)']):
+                        continue
 
-                if validate_line.contain(line, ['|', '{{', ':', '[', 'ISBN', '#', 'REDIRECT', 'isbn', 'RINVIA']):
-                    continue
+                    if validate_line.contain(line, ['|', '{{', ':', '[', 'ISBN', '#', 'REDIRECT', 'isbn', 'RINVIA']):
+                        continue
 
-                if validate_line.isdigit([line, line[1:], line[:1]]):
-                    continue
+                    if validate_line.isdigit([line, line[1:], line[:1]]):
+                        continue
 
-                if validate_line.isbookref(line):
-                    continue
+                    if validate_line.isbookref(line):
+                        continue
 
-                if validate_line.isbrokensimplebracket(line):
-                    continue
-                # a gentle filter that just allow the italian alphabet chars and
-                # prevents lines made up by just symbols (eg ........ )
-                if re.search("[aAàÀbBcCdDeEèÈéÉfFgGhHiIìÌjJkKlLmMnNoOòÒpPqQrRsStTuUùÙvVwWxXyYzZ]+",line):
-                    text += line + "\n"
+                    if validate_line.isbrokensimplebracket(line):
+                        continue
+                    # a gentle filter that just allow the italian alphabet chars and
+                    # prevents lines made up by just symbols (eg ........ )
+                    if re.search("[aAàÀbBcCdDeEèÈéÉfFgGhHiIìÌjJkKlLmMnNoOòÒpPqQrRsStTuUùÙvVwWxXyYzZ]+",line):
+                        text += line + "\n"
 
-            result.write(text)
+                result.write(text)
 
 result.close()
 
