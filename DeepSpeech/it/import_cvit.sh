39d85622ca9eb9d66de6de7f4183d2b2c16fcbb2
Daniele Scasciafratte <mte90net@gmail.com>
2020-07-31 18:38:38 +0200

    not check checksum if folder already extracted

--- DeepSpeech/it/import_cvit.sh
+++ DeepSpeech/it/import_cvit.sh
@@ -8,16 +8,16 @@ pushd $HOME/ds/
 			exit 1
 	fi;
 
-	if ! echo "a304332d1dfdb772ae75859441f46a88438305d2f5420680c3c64fba62ea6830  /mnt/sources/it.tar.gz" | sha256sum --check; then
-		echo "Invalid Common Voice IT dataset"
-		exit 1
-	fi;
-
-	if [ "${ENGLISH_COMPATIBLE}" = "1" ]; then
-		IMPORT_AS_ENGLISH="--normalize"
-	fi;
-
 	if [ ! -f "/mnt/extracted/data/cv-it/clips/train.csv" ]; then
+        if ! echo "a304332d1dfdb772ae75859441f46a88438305d2f5420680c3c64fba62ea6830  /mnt/sources/it.tar.gz" | sha256sum --check; then
+            echo "Invalid Common Voice IT dataset"
+            exit 1
+        fi;
+
+        if [ "${ENGLISH_COMPATIBLE}" = "1" ]; then
+            IMPORT_AS_ENGLISH="--normalize"
+        fi;
+        
 		mkdir -p /mnt/extracted/data/cv-it/ || true
 
 		tar -C /mnt/extracted/data/cv-it/ --strip-components=2 -xf /mnt/sources/it.tar.gz
