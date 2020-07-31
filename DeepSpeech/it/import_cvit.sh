b2e18ce21f53e7d53da33281f7d90b9f5eb9c76e
Daniele Scasciafratte <mte90net@gmail.com>
2020-07-31 15:44:09 +0200

    trying to fix checksum validation of cvit

--- DeepSpeech/it/import_cvit.sh
+++ DeepSpeech/it/import_cvit.sh
@@ -8,9 +8,7 @@ pushd $HOME/ds/
 			exit 1
 	fi;
 
-	sha256=$(sha256sum --binary /mnt/sources/it.tar.gz | awk '{ print $1 }')
-
-	if [ "${sha256}" != "a304332d1dfdb772ae75859441f46a88438305d2f5420680c3c64fba62ea6830" ]; then
+	if [ ! echo "a304332d1dfdb772ae75859441f46a88438305d2f5420680c3c64fba62ea6830  /mnt/sources/it.tar.gz" | sha256sum --check ]; then
 		echo "Invalid Common Voice IT dataset"
 		exit 1
 	fi;
