11372b93365ddcfda711ffdb61e17ffa408a0155
eziolotta <ezio.dinisio@gmail.com>
2020-12-06 23:40:36 +0100

    standard colname for csv exported

--- MITADS-Speech/lablita_importer.py
+++ MITADS-Speech/lablita_importer.py
@@ -39,7 +39,7 @@ def main():
     outfilepath = output_ipic_folder+ 'lablita_corpus.csv'
     csv_file = open(outfilepath,"w+",encoding="utf8")
     fh_out = csv.writer(csv_file,quoting=csv.QUOTE_NONNUMERIC)
-    fh_out.writerow(['mp3_filename','mp3_filesize','transcript','transcript_annotated'])
+    fh_out.writerow(['filename','filesize','transcript','transcript_annotated'])
 
     #to improve performance we write on the append file for the whole download, but comparisons test needed
     #csv_file.close()
