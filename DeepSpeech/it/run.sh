2623ec9ebee312e4158d14a4ea0f9caa4c0b3e02
nefastosaturo <10575994+nefastosaturo@users.noreply.github.com>
2021-02-09 18:00:25 +0100

    voxforge importer through the official one (#121)
    * add voxforge with blacklist
    
    * add import_voxforge into run.sh
    
    * fix double remove same blacklisted speaker

--- DeepSpeech/it/run.sh
+++ DeepSpeech/it/run.sh
@@ -8,6 +8,7 @@ if [ "${FAST_TRAIN}" = 1 ]; then
   ${MODEL_LANGUAGE}/import_cvit_tiny.sh
   export BATCH_SIZE=2
 else
+  ${MODEL_LANGUAGE}/import_voxforge.sh
   ${MODEL_LANGUAGE}/import_cvit.sh
   ${MODEL_LANGUAGE}/import_m-ailabs.sh
 fi
