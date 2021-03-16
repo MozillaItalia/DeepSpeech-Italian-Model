184d30d95c68abbb20018c26c28cb0ac0060ea2a
eziolotta <ezio.dinisio@gmail.com>
2021-03-16 12:42:04 +0100

    fix siwis transcript

--- MITADS-Speech/siwis_importer.py
+++ MITADS-Speech/siwis_importer.py
@@ -18,7 +18,21 @@ class SiwisImporter(ArchiveImporter):
         audios = []
         wav_dir  = os.path.join(self.origin_data_path, self.extract_dir, "wav","IT")
         text_dir = os.path.join(self.origin_data_path, self.extract_dir, "txt","IT")
-        
+        ##read transcript in prompts.txt
+        transcripts = {}
+        ##cp1252 if windows os
+        encoding = 'cp1252' if os.name == 'nt' else 'utf-8'
+        ###read transcript from prompts file
+        with open(os.path.join(self.origin_data_path,self.extract_dir, "prompts","ALL_IT_prompts_iso.txt"), "r",encoding=encoding) as f:
+            line = f.readline()
+            while line:
+                temp = re.split(r'\t', line)
+                filename = temp[0]
+                transcript = temp[1].strip()
+                transcripts[filename] = transcript
+                # use realine() to read next line
+                line = f.readline()
+
         for subdir, dirs, files in os.walk(wav_dir):
             uuu=0
             for _dir in dirs:
@@ -28,38 +42,26 @@ class SiwisImporter(ArchiveImporter):
                     continue
 
                 curr_wav_dir =  os.path.join(subdir, _dir)
-                curr_txt_dir =  os.path.join(text_dir, _dir)
-
+                #
                 ##iterate wav file current folder
                 for fname in os.listdir(curr_wav_dir):
                     fname = os.fsdecode(fname)
-
                     if(fname=='converted'):
-                        ##wav converted in a previous session run
+                        ##skip wav converted by importer
                         continue
+                    wav_file_path = os.path.join(wav_dir, _dir,fname) 
 
-                    wav_file_path = os.path.join(wav_dir, _dir,fname)                        
-                    txt_file_path = os.path.join(curr_txt_dir, fname.split('.')[0]+'.txt')
-                    if(not os.path.isfile(txt_file_path)):
-                        print('audio file {} doesn\'t have a file transcript'.format(wav_file_path))
-                        continue
-
-                    ##read file transcript
-                    transcript = ''
-
-                    ##files have different encoding (utf-8, utf_16_be, etc..)
-                    ##need check to open file with correct encoding
-                    file_encoding ='utf-8'                   
-                    enc = CnM.from_path(txt_file_path).best().first()
-                    file_encoding = enc.encoding
-                    ##fix same encoding 
-                    if(file_encoding=='big5' or file_encoding=='cp1252' ):
-                        file_encoding = 'utf-8'                    
-
-                    with open(txt_file_path, "r",encoding=file_encoding) as f:
-                        transcript += f.readline()
-
-                    transcript = transcript.strip()
+                    try:
+                        transcript = transcripts[fname.replace('.wav','.txt')]
+                    except:                        
+                        curr_txt_dir =  os.path.join(text_dir, _dir)
+                        txt_file_path = os.path.join(curr_txt_dir, fname.split('.')[0]+'.txt')
+                        #print('missing prompts , read transcript from file {}'.format(txt_file_path))
+                        if(not os.path.isfile(txt_file_path)):
+                            raise ValueError('audio file {} doesn\'t have a file transcript'.format(wav_file_path))  
+                            #continue
+                        transcript = self.read_txt_file(txt_file_path) 
+            
                     ##append data manifest
                     utterances[wav_file_path] = transcript
                     audios.append(wav_file_path)     
@@ -71,6 +73,24 @@ class SiwisImporter(ArchiveImporter):
         ## not require resample
         corpus.make_wav_resample = True
         return corpus
+
+    def read_txt_file(self,txt_file_path):
+        transcript = ''
+
+        ##files have different encoding (utf-8, utf_16_be, etc..)
+        ##need check to open file with correct encoding
+        file_encoding ='utf-8'                   
+        enc = CnM.from_path(txt_file_path).best().first()
+        file_encoding = enc.encoding
+        ##fix same encoding 
+        if(file_encoding=='big5' or file_encoding=='cp1252' ):
+            file_encoding = 'utf-8'                    
+
+        with open(txt_file_path, "r",encoding=file_encoding) as f:
+            transcript += f.readline()
+
+        transcript = transcript.strip()
+        return transcript
 
     def get_speaker_id(self,audio_file_path):
     
@@ -84,12 +104,6 @@ class SiwisImporter(ArchiveImporter):
     # or None if it's invalid.
     def validate_label(self,label):
 
-
-
-
-        # For now we can only handle [a-z ']
-
-
         label = label.replace("-", " ")
         label = label.replace("_", " ")
         label = re.sub("[ ]{2,}", " ", label)
@@ -100,20 +114,17 @@ class SiwisImporter(ArchiveImporter):
         label = label.replace("!", "")
         label = label.replace(":", "")
         label = label.replace("\"", "")
-        ##
-        label = label.replace("î", "i")  
-        label = label.replace("û", "u") 
-        label = label.replace("ã", "a") 
+        ##        
 
         label = label.replace("’", "\'") 
-        label = label.replace("´", "\'") 
- 
+        label = label.replace("´", "\'")  
 
         label = label.replace("»", "") 
         label = label.replace("«", "") 
-        #label = label.replace("ã", "a") 
-        #label = label.replace("ã", "a") 
-
+        ############################
+        label = label.replace("Ã", "a")##present , see 'domandÃ'
+        ####
+        
         label = label.replace("²", "") 
         label = label.replace("<", "")  
         label = label.replace(">", "") 
@@ -142,8 +153,7 @@ class SiwisImporter(ArchiveImporter):
         label = label.replace("2%", "due per cento")
         label = label.replace("741", "settecentoquarantuno")
         label = label.replace("103", "settecentoquarantuno")
-
-
+        ######################## 
 
         if re.search(r"[0-9]|[\[\]&*{]", label) is not None:
             return None
@@ -151,12 +161,6 @@ class SiwisImporter(ArchiveImporter):
 
         label = label.strip()
         label = label.lower()
-
-        ##DEBUG - decomment for checking normalization char by char
-        #from corpora_importer import DEBUG_ALPHABET
-        #for c in label:
-        #    if(c not in DEBUG_ALPHABET):
-        #        print('CHECK char:'+ c)
 
         return label if label else None
 
